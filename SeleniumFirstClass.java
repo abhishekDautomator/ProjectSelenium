0c3827fe9557a74f2233ecdda2d89c47104fc12e
Abhishek narayan <41482419+abhishekDautomator@users.noreply.github.com>
2018-07-24 22:42:42 +0530

    Add files via upload

---
+++ SeleniumFirstClass.java
@@ -0,0 +1,14 @@
+package learnSelenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		static WebDriver driver;
+
+	}
+
+}
